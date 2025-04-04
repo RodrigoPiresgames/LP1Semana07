228a41b0c0139e4ede6a96054bd9ad0786e0211a
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-04-04 12:42:27 +0100

    Changed tp private variables

---
+++ ColorSpheres/Sphere.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Threading.Tasks;
+ 
+ namespace ColorSpheres
+ {
+     public class Sphere
+     {
+         private byte cor;
+         private int raio;
+         private int atirado;
+ 
+     }
+ }
